2bb03eeeefe2566b8312f7df9672c1eddded205e
default <william.provost@stu.ocsb.ca>
2025-11-30 20:16:31 +0000

    fixed linting error

--- Unit4.ts
+++ Unit4.ts
@@ -6,17 +6,17 @@
  */
 
 // variables
-let numberAsString: string = "";
-let numberAsNumber: number = 0;
+let numberAsString1: string = "";
+let numberAsNumber1: number = 0;
 
 // input number
-numberAsString = prompt("Enter a number (1-12): ") || "0";
-numberAsNumber = parseInt(numberAsString);
+numberAsString1 = prompt("Enter a number (1-12): ") || "0";
+numberAsNumber1 = parseInt(numberAsString1);
 
 // print times table
-console.log(`\nTimes Table for ${numberAsNumber}:`);
+console.log(`\nTimes Table for ${numberAsNumber1}:`);
 for (let counter: number = 1; counter <= 12; counter++) {
-  console.log(`${numberAsNumber} x ${counter} = ${numberAsNumber * counter}`);
+  console.log(`${numberAsNumber1} x ${counter} = ${numberAsNumber1 * counter}`);
 }
 
 console.log("\nDone.");
